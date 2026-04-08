4ce0b9bdc18b60eece0c570fd2b6cffa0d987ae1
reka-kovacs <61095901+reka-kovacs@users.noreply.github.com>
2026-04-08 14:13:41 -0600

    cleanup

--- CREATE TYPE dbo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE dbo.CourseProgressType AS TABLE (
-    participant_id INT,
-    course_id INT,
-    course_title VARCHAR(MAX),
-    first_accessed DATETIMEOFFSET(7),
-    last_accessed DATETIMEOFFSET(7),
-    completion FLOAT
-);
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import sql from "mssql";
-import { config } from "../config.js";
+import { config } from "./config.js";
 
 let pool;
 export async function getConnection() {
